8f9c638412d8664111ccd7deebcda524d1f9bc5c
yatoro667 <ytorres667@gmail.com>
2021-02-04 03:29:13 +0000

    Betty is pleased

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
 * main - writes in unicode
 *
-* Return: 0
+* Return: 1
 */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
-	return (0);
+	return (1);
 }
