6ce19be8d8c10e267c92fe9e455712265685bd4c
yatoro667 <ytorres667@gmail.com>
2021-02-05 20:32:33 +0000

    Fixed code on 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,22 +10,22 @@ int main(void)
 
 	int d1, d2, d3;
 
-	for (d1 = 0; d1 <= 7; d1++)
+	for (d1 = '0'; d1 <= '7'; d1++)
 	{
-		for (d2 = d1 + 1; d2 <= 8; d2++)
+		for (d2 = '0'; d2 <= '8'; d2++)
 		{
-			for (d3 = d2 + 1; d3 <= 9; d3++)
+			for (d3 = '0'; d3 <= '9'; d3++)
 			{
-				if ((d1 > d2) && (d2 > d3))
+				if ((d1 < d2) && (d2 < d3))
 				{
-					putchar(d1);
-					putchar(d2);
-					putchar(d3);
-				}
-				if (d1 != 7)
-				{
+				putchar(d1);
+				putchar(d2);
+				putchar(d3);
+					if (d1 != '7')
+					{
 					putchar(',');
 					putchar(' ');
+					}
 				}
 			}
 		}
