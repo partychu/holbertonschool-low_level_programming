d1c230e238a566714970cef8f37f9057953f976d
yatoro667 <ytorres667@gmail.com>
2021-02-05 14:47:07 +0000

    testingcheck

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints 00 - 99
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if (x < '9' || y < '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet no qe
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'q' || x != 'e')
+			continue;
+
+		putchar(x);
+	}
+
+	putchar('\n');
+	return (0);
+}
