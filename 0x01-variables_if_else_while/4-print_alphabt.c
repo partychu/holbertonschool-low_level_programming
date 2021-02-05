2c1d595bac057ae347f6e06be07693a69109cb66
yatoro667 <ytorres667@gmail.com>
2021-02-05 15:49:58 +0000

    Adding all mandatory

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,12 @@ int main(void)
 	char x;
 
 	for (x = 'a'; x <= 'z'; x++)
+	if (x != 'q' || x != 'e')
+
 	{
 		putchar(x);
-
-		if (x != 'q' || x != 'e')
-			continue;
-		{
-		putchar(x);
-		}
 	}
+
 	putchar('\n');
 	return (0);
 }
