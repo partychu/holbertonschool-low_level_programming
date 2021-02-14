ace59adbac540d9dfc3dd08ba4643ab2bd51e381
yatoro667 <ytorres667@gmail.com>
2021-02-14 03:44:57 +0000

    fixing 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,9 +1,20 @@
-void print_square(int size);
+#include "holberton.h"
 
-if (size > 0)
-	for (sq1 = 0; sq1 < size; sq1++)
-		for (sq2 = 0; sq2 < size; sq2++)
-		_putchar('#');
+void print_square(int size)
+{
+	int sq1, sq2;
+
+	if (size > 0)
+	{
+		for (sq1 = 0; sq1 < size; sq1++)
+		{
+			for (sq2 = 0; sq2 < size; sq2++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
 		_putchar('\n');
-
-(_putchar('\n');
+}
