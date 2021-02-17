85bb2d59c7b458a463324eb40c93840ea0d75aaf
yatoro667 <ytorres667@gmail.com>
2021-02-17 07:45:21 +0000

    Adding 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,21 +2,46 @@
 
 /**
  * rev_string - reverses a string
- *
  * @s: string to be reversed
+ * Return: rev string
  */
 
+int string_length(char *s);
 void rev_string(char *s)
-}
-	int i;
-	int j;
-	char temp;
+{
+	int strl, c;
+	char *first, *last, tmp;
 
-	for (i = 0; s[i] != '\0'; i++)
+	strl = string_length(s);
+	first = s;
+	last = s;
+
+	for (c = 0; c < strl - 1; c++)
+		last++;
+
+	for (c = 0; c < strl / 2; c++)
 	{
+		tmp = *last;
+		*last = *first;
+		*first = tmp;
+
+		first++;
+		last--;
 	}
-	
-	j = i - 1
-	{
-		if
+}
 
+/**
+ * string_length - returns length of string
+ * @s: string to find length of
+ * Return: c
+ */
+
+int string_length(char *s)
+{
+	int c = 0;
+
+	while(*(s + c) != '\0')
+		c++;
+
+	return (c);
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * puts2 - printers every other line char of a string
+ *
+ * @str: string to be modified
+ */
+
+void puts2(char *str)
+
+
