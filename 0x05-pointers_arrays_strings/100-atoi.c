715adbe40b8e381d3d0b62109b8a0846a01c43e0
yatoro667 <ytorres667@gmail.com>
2021-02-17 23:42:49 +0000

    Editing

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,7 +21,7 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			num = num * 10 + sign * (s[i] - '0');
-			if (s[i] >= '0' && s[i] <= '9')
+			if (s[i + 1] >= '0' && s[i + 1] <= '9')
 				end = 0;
 			else
 				end = 1;
