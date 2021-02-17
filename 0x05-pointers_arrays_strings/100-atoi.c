7757492e96acab7864eb4460a96bacd6a6d414d1
yatoro667 <ytorres667@gmail.com>
2021-02-17 21:50:07 +0000

    Adding 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * atoi - converts as tring to an integer
+ * @s: string to be converted
+ */
+
+int _atoi(char *s);
+{
+	int i = 0, strl = 0, num = 0, sign = 1, cut = 0;
+
+	while (s[strl] != '\0')
+		len++;
+	while (i < strl && ok == 0)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+			num = num * 10 + (s[i] - '0')
+	}
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * print_array - prints n elements of an array of ints
+ * @n: number of elements to print
+ */
+
+void print_array(int *a, int n)
+{
+
