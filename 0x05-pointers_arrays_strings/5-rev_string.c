afc8ec1084f5523151974516fd513ea81bba39fe
yatoro667 <ytorres667@gmail.com>
2021-02-17 03:54:28 +0000

    Quick commit b4 deadline

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+}
+	int i;
+	int j;
+	char temp;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	
+	j = i - 1
+	{
+		if
+
