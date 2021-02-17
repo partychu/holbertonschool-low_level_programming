a7120c5f47c14d0608449246819aaa15026cb54e
yatoro667 <ytorres667@gmail.com>
2021-02-17 17:08:15 +0000

    Adding

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints latter half of a string
+ * @str: string to be altered
+ */
+
+void puts_half(char *str)
+{
+	int i, n;
+
+	while (str[i] != '\0')
+		i++;
+	if (i % 2 == 0)
+	{
+		for (n = i / 2; n < i; n++)
+			_putchar(str[n]);
+	}
+	else
+	{
+		for (n = (i - 1) / 2; n > i; n++)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
+}
