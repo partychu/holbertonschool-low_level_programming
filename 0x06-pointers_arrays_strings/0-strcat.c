7481165fbcb7084c4f9b03243ccfbcff52660794
yatoro667 <ytorres667@gmail.com>
2021-02-18 09:20:23 +0000

    Adding 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination str
+ * @src: source string
+ * Return: returns concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
+	return (dest);
+}
+
