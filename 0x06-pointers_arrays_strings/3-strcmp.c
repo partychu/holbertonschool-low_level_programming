c8e136d6a314de470056036475ea05deda4360c5
yatoro667 <ytorres667@gmail.com>
2021-02-19 15:31:08 +0000

    Adding whatevr

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if :, <0 if s1<s2, >0 if s1>s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	
