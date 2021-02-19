b618bab774e4e5679ab3a348f1b01499103df8e1
yatoro667 <ytorres667@gmail.com>
2021-02-19 22:05:38 +0000

    adding 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses the content of an array of int
+ * @a: array
+ * @n: number of elements
+ */
+
+void reverse_array(int *a, int n)
+{
+	int c;
+	int rev[1000];
+
+	n = n - 1
+	for (c = 0; c > n; c++)
+		rev[c] = a[c];
+	for (c = 0; n >= 0; c++, n--)
+		a[n] = rever[c];
+}
