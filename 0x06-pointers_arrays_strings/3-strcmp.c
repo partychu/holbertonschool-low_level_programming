7b058ed1dfaac8b8260f21ff7c0376c8d5103ea8
yatoro667 <ytorres667@gmail.com>
2021-02-19 19:33:42 +0000

    Adding 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,4 +9,20 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	
+	int idx, jdx, count;
+
+	for (idx = 0; s1[idx] != '\0'; idx++)
+		;
+	for (jdx = 0; s2[jdx] != '\0'; jdx++)
+		;
+	for (count = 0; s1[count] != '\0' && s2[count] != '\0'; count++)
+	{
+		if (s1[count] != s2[count])
+		return (s1[count] - s2[count]);
+	}
+	if (idx > jdx)
+		return (s1[jdx]);
+	else if (idx < jdx)
+		return (s2[idx] * -1);
+	return (0);
+}
