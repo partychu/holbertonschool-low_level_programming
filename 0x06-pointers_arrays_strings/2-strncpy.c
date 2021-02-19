6e019aab86c8c3753503f80635f90d85febbe602
yatoro667 <ytorres667@gmail.com>
2021-02-19 03:51:55 +0000

    fixing errors

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int idx
+	int idx;
 
-	for (idx = 0; i < n && src[idx] != '\0'; i++)
+	for (idx = 0; idx < n && src[idx] != '\0'; idx++)
+	{
 		dest[i] = src[i];
-	for ( ; idx < n; idk++)
+	}
+	for (; idx < n; idx++)
+	{
 		dest[i] = '\0';
+	}
 	return (dest);
 }
 
