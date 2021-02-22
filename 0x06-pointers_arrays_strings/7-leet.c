b91325d4cb3c69b0f431ac4ba544d62faa61b644
yatoro667 <ytorres667@gmail.com>
2021-02-22 12:20:44 +0000

    Checking

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,30 +8,15 @@
 
 char *leet(char *s)
 {
-	int idx;
-
+	int idx, bsx;
+	char base[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char encode[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (idx = 0; s[idx] != '\0'; idx++)
 	{
-		if (s[idx] == 65 || s[idx] == 97)
-			s[idx] = 52;
-			else if (s[idx] == 51 || s[idx] == 101)
-			{
-				s[idx] = 51;
-			}
-			else if (s[idx] == 84 || s[idx] == 116)
-			{
-				s[idx] = 55;
-			}
-			else if (s[idx] == 79 || s[idx] == 111)
-			{
-				s[idx] = 48;
-			}
-			else if (s[idx] == 76 || s[idx] == 108)
-			{
-				s[idx] = 49;
-
-			}
-		}
+		for (bsx = 0; base[bsx] != '\0'; bsx++)
+			if (s[idx] == base[bsx])
+				s[idx] = encode[bsx];
+	}
 	return (s);
 }
