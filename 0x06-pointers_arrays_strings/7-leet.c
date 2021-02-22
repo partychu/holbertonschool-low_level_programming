5aa4e14e7900935ffb16b501866989767e1d70f2
yatoro667 <ytorres667@gmail.com>
2021-02-22 09:39:20 +0000

    Correct out put

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,13 +9,18 @@ char *leet(char *s)
 {
 	int idx;
 
-	for (idx = 0; s[idx] != '\0'; idx++){
-			if (s[idx] == 65 || 97)
-				s[idx] = 52;
-			if (s[idx] == 51 || 101)
+	for (idx = 0; s[idx] != '\0'; idx++)
+	{
+		if (s[idx] == 65 || s[idx] == 97)
+		{
+			s[idx] = 52;
+			else if (s[idx] == 51 || s[idx] == 101)
 				s[idx] = 51;
-			if (s[idx] == 84 || 116)
+			else if (s[idx] == 84 || s[idx] == 116)
 				s[idx] = 55;
-	}
+			else if (s[idx] == 79 || s[idx] == 108)
+				s[idx] = 48;
+			else if (s[idx] == 76 || s[idx] == 108)
+				s[idx] = 49;
 	return (s);
 }
