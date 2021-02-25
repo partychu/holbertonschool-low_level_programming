5e09841cd98025933114ad53c9fbe99359563ef5
yatoro667 <ytorres667@gmail.com>
2021-02-25 04:21:51 +0000

    Optimization for 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,12 +8,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
 }
 
