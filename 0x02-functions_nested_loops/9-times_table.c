8f0ef9534d5c0704086852b9d0907ba6e684c6a2
yatoro667 <ytorres667@gmail.com>
2021-03-06 18:01:13 +0000

    Adding 9 lol

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int num, mul;
+	int result;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (mul = 0; mul <= 9; mul++)
+		{
+			result = mul * num;
+			if (result >= 10)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else if (mul < 10)
+				_putchar(result + '0');
+			if (mul < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
