81738336c12906d843a61bb73fb11cf38b074d28
yatoro667 <ytorres667@gmail.com>
2021-03-10 18:17:59 +0000

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,16 +12,17 @@ void print_triangle(int size)
 	{
 		int x, y;
 
-		for (y = 0, y < size, y++)
+		for (y = 0; y < size; y++)
 		{
-			for (x = 1; x < size, x++)
+			for (x = 1; x < (size - y); x++)
 			{
 				_putchar(' ');
 			}
-				for (; x <= size; x++)
-				{
-					_putchar('#');
-				}
+			for (; x <= size; x++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
 	}
 	else
