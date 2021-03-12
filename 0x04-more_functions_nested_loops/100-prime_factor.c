4f5bd20d37701c25714394b9fbd9e2727d9ed74b
yatoro667 <ytorres667@gmail.com>
2021-03-12 13:57:17 +0000

    add 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,19 +8,19 @@
 
 int main(void)
 {
-long big = 612852475143;
-long f;
-long pf;
+	long big = 612852475143;
+	long f;
+	long pf;
 
 	for (f = 2; f <= big; f++)
 	{
 		while (big % f == 0)
 		{
-			big = big / f
-		if (f > pf)
-			pf = f
+			big = big / f;
+			if (f > pf)
+				pf = f;
 		}
 	}
-	printf("%d", pf);
+	printf("%ld\n", pf);
 	return (0);
 }
