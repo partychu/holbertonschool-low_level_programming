f3ea4d01451c80b3194ac431c68ea48472090715
yatoro667 <ytorres667@gmail.com>
2021-03-19 17:44:03 +0000

    Testing 4 on checker

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head - first node of list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	tmp = head->next;
+	free(head->str);
+	free(head);
+	head = tmp;
+}
