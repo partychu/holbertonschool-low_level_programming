09776a44119d165bad60d313a544f3cb4c916768
yatoro667 <ytorres667@gmail.com>
2021-03-24 08:16:43 +0000

    checking 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes all words
+ * @str: string to capitalize
+ * Return: modified string
+ */
+
+char *cap_string(char *str)
+{
+	int idx;
+
+	for (idx = 0; idx != '\0'; idx++)
+	{
+		while (str[idx] >= 'a' && str[idx] <= 'z')
+		
+			if (idx == 0 || str[idx - 1] == ' ' ||
+				str[idx - 1] == '\t' || str[idx - 1] == '\n' ||
+				str[idx - 1] == ',' || str[idx - 1] == ';' ||
+				str[idx - 1] == '.' || str[idx - 1] == '!' ||
+				str[idx - 1] == '?' || str[idx - 1] == '"' ||
+				str[idx - 1] == '(' || str[idx - 1] == ')' ||
+				str[idx - 1] == '{' || str[idx - 1] == '}')
+				str[idx] -= 32;
+	}
+	return (str);
+}
