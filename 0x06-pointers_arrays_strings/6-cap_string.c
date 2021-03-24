c0cad026ac94c187073701a2df44d7f228c5ca71
yatoro667 <ytorres667@gmail.com>
2021-03-24 18:52:41 +0000

    idk

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,18 +10,24 @@ char *cap_string(char *str)
 {
 	int idx;
 
-	for (idx = 0; idx != '\0'; idx++)
+	for (idx = 0; str[idx] != '\0'; idx++)
 	{
 		while (str[idx] >= 'a' && str[idx] <= 'z')
-		
-			if (idx == 0 || str[idx - 1] == ' ' ||
-				str[idx - 1] == '\t' || str[idx - 1] == '\n' ||
-				str[idx - 1] == ',' || str[idx - 1] == ';' ||
-				str[idx - 1] == '.' || str[idx - 1] == '!' ||
-				str[idx - 1] == '?' || str[idx - 1] == '"' ||
-				str[idx - 1] == '(' || str[idx - 1] == ')' ||
-				str[idx - 1] == '{' || str[idx - 1] == '}')
+			if (str[idx - 1] == ' ' ||
+					str[idx - 1] == '\n' || str[idx - 1] == '\t' ||
+					str[idx - 1] == ',' || str[idx - 1] == ';' ||
+					str[idx - 1] == '.' || str[idx - 1] == '!' ||
+					str[idx - 1] == '?' || str[idx - 1] == '"' ||
+					str[idx - 1] == '(' || str[idx - 1] == ')' ||
+					str[idx - 1] == '{' || str[idx - 1] == '}' ||
+					idx == 0)
+			{
 				str[idx] -= 32;
+			}
+			else
+			{
+				idx++;
+			}
 	}
 	return (str);
 }
