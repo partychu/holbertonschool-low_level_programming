a73cd7da2f8e3661b3d2da5a6b0b30fcdb6c710d
yatoro667 <ytorres667@gmail.com>
2021-03-26 15:04:31 +0000

    Adding 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints binary of a num
+ * @n: number to be conv
+ */
+
+void print_binary(unsigned long int n);
+{
+	if ((n >> 1 ) != 0)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
