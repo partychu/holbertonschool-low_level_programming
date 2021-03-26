d03483eb41dffe4090b925d628d59fd212e0a517
yatoro667 <ytorres667@gmail.com>
2021-03-26 14:37:10 +0000

    Adding 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - checks endianness
+ * Return: 0 big endian, 1 little endian
+ */
+int get_endianness(void)
+{
+	unsigned int check = 1;
+	
+	return ((int) (((char *)&check)[0]));
+}
