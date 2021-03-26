e2e7af414bc4bf88e8f2d320fcd89b9875d11274
yatoro667 <ytorres667@gmail.com>
2021-03-26 16:33:08 +0000

    Fixing 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,9 +5,9 @@
  * @n: number to be conv
  */
 
-void print_binary(unsigned long int n);
+void print_binary(unsigned long int n)
 {
-	if ((n >> 1 ) != 0)
+	if ((n >> 1) != 0)
 		print_binary(n >> 1);
 	_putchar((n & 1) + '0');
 }
