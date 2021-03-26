a755952e3350a8c7fadcfddc3748e484a20aa141
yatoro667 <ytorres667@gmail.com>
2021-03-26 18:03:35 +0000

    Adding 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * get_bit - returns value of a bit at given index
+ * @index: index
+ * @n: bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > (sizeof(n) * 8))
+	{
+		return (-1);
+	}
+	return ((n >> index) & 1);
+}
