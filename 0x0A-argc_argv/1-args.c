d3373c43c3cb77eb5f6722cde828c7dc1996fecc
yatoro667 <ytorres667@gmail.com>
2021-04-12 04:30:24 +0000

    check 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - prints # of args
+ * @argc: ac
+ * @argv: av
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
