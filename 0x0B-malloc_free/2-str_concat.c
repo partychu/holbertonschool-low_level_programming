3e9008e04b9bcacdef2ea7742bb7a52009a5f73c
yatoro667 <ytorres667@gmail.com>
2021-04-15 10:02:12 +0000

    Create str_concat function

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * str_concat - concatenates two strings and allocates memory
+ * @s1: destination
+ * @s2: source
+ * Return: pointer to new str, NULL on failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len, idx;
+	char *catstr;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (idx = 0; s1[idx] != '\0'; idx++)
+		;
+	len = idx;
+	for (idx = 0; s2[idx] != '\0'; idx++)
+		;
+	len += idx;
+
+	catstr = malloc(sizeof(char) * len);
+
+	if (catstr == NULL)
+		return (NULL);
+
+	for (idx = 0; s1[idx] != '\0'; idx++)
+	{
+		catstr[idx] = s1[idx];
+	}
+	len = idx;
+	for (idx = 0; s2[idx] != '\0'; idx++)
+	{
+		catstr[len] = s2[idx];
+		len++;
+	}
+	catstr[len] = '\0';
+	return (catstr);
+}
