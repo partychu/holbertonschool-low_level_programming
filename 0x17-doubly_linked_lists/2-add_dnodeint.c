7f97e44c9e802ebe5743fd904ea76e9999ed1913
yatoro667 <ytorres667@gmail.com>
2021-05-03 08:52:08 +0000

    Rechecking

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,9 +11,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
@@ -22,9 +19,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
-	
-	if (*head)
-		head->prev = new;
 
 	*head = new;
 
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
-	
+
 	if (new == NULL)
 		return (NULL);
 
@@ -27,7 +27,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 
 	pointer = *head;
-	
+
 	while (pointer->next != NULL)
 		pointer = pointer->next;
 
