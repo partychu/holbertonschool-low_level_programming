d7cde2bdc4b6e8090bb103f1fc789319368029fc
yatoro667 <ytorres667@gmail.com>
2021-05-03 18:12:01 +0000

    Rechecking 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,9 +11,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new, *pointer;
 
-	if (head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
