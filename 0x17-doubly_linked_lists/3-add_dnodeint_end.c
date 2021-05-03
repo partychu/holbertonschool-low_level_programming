39b18542a3d3d872416a5a99b3aa2a1ec4d76d43
yatoro667 <ytorres667@gmail.com>
2021-05-03 17:02:44 +0000

    Betty fix

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -31,12 +31,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	pointer = *head;
 	new->prev = pointer;
-	
+
 	while (pointer->next != NULL)
 		pointer = pointer->next;
 
 	pointer->next = new;
-
 
 	return (new);
 }
