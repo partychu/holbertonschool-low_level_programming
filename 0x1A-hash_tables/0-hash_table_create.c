834f17c7ca9905836efbbed8a5c7ccc1ff689711
yatoro667 <ytorres667@gmail.com>
2021-05-29 00:25:32 +0000

    Betty fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 
 	ht = malloc(sizeof(hash_table_t));
-	
+
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
 	ht->array = calloc(size, sizeof(hash_node_t *));
-	
+
 	if (ht->array == NULL)
 	{
 		free(ht);
