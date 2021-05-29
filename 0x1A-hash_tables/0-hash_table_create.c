9c9222833660a8a3f35c747cc7558a180a870190
yatoro667 <ytorres667@gmail.com>
2021-05-29 00:24:20 +0000

    Checking 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,5 +8,21 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *ht;
 
+	ht = malloc(sizeof(hash_table_t));
+	
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	return (ht);
 }
