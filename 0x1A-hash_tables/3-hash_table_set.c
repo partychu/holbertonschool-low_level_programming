ab8781744f70cd801458eb69e54ccf61e41bba18
yatoro667 <ytorres667@gmail.com>
2021-06-02 01:31:03 +0000

    Fix 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hast_table_set - adds element to hash table
+ * hash_table_set - adds element to hash table
  * @ht: hash table
  * @key: key
  * @value: value associated with key
@@ -10,25 +10,13 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *h_node;
+	hash_node_t *new_node;
 	unsigned long int idx = 0;
 
 	if (key == NULL || value == NULL || ht == NULL || (strcmp(key, "") == 0))
 		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
-
-	h_node = ht->array[idx];
-
-	while (h_node)
-	{
-		if (strcmp(h_node->key, key))
-		{
-			free(h_node->value);
-			h_node->value = strdup(value);
-		}
-		h_node = h_node->next;
-	}
 
 	new_node = malloc(sizeof(hash_node_t *));
 
@@ -37,7 +25,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = ht->array[idx];
+
+	if (ht->array[idx] == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = ht->array[idx];
+
 	ht->array[idx] = new_node;
 
 	return (1);
