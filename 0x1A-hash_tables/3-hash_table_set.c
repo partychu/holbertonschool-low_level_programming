d27cf610d00eea762aac533d94ac6e0e0f09b34f
yatoro667 <ytorres667@gmail.com>
2021-06-02 01:21:50 +0000

    checking 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,28 +10,36 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
+	hash_node_t *new_node, *h_node;
 	unsigned long int idx = 0;
 
 	if (key == NULL || value == NULL || ht == NULL || (strcmp(key, "") == 0))
 		return (0);
 
-	new_node = malloc(sizeof(hash_node_t *));
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	h_node = ht->array[idx];
+
+	while (h_node)
+	{
+		if (strcmp(h_node->key, key))
+		{
+			free(h_node->value);
+			h_node->value = strdup(value);
+		}
+		h_node = h_node->next;
+	}
+
 		
+	new_node = malloc(sizeof(hash_node_t *));
+
 	if (new_node == NULL)
 		return (0);
 
-	new_node->key = (char *)key;
+	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-
-	idx = key_index((const unsigned char *)key, ht->size);
-
-	if (ht->array[idx] == NULL)
-		new_node->next = NULL;
-	else
-		new_node->next = ht->array[idx];
-
+	new_node->next = ht->array[idx];
 	ht->array[idx] = new_node;
-	
+
 	return (1);
 }
