a4dd34205e6487bd748ad89e1cfcfa2f24e93b46
yatoro667 <ytorres667@gmail.com>
2021-06-02 02:52:07 +0000

    Checking 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,22 +8,22 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *pointer;
-	unsigned long int i;
-	int flag;
+	unsigned long int i, flag = 0;
 
 	if (ht == NULL)
-		printf("");
+		return;
 
 	printf("{");
 
 	for (i = 0; i < ht->size; i++)
 	{
 		pointer = ht->array[i];
-		flag = 0;
 		while (pointer)
 		{
 			if (flag == 1)
+			{
 				printf(", ");
+			}
 			printf("'%s': '%s'", pointer->key, pointer->value);
 			pointer = pointer->next;
 			flag = 1;
