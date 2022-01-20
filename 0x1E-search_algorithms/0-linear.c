8fceb942b82b320d45ad7478e087e339c0cda390
partychu <ytorres667@gmail.com>
2022-01-20 04:12:15 +0000

    Betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,18 +12,16 @@
 int linear_search(int *array, size_t size, int value)
 {
 
-    size_t i;
-    int tmp;
+size_t i;
 
-    if (array != NULL)
-    {
-        for (i = 0; i < size; i++)
-        {
-            tmp = array[i];
-            printf("Value checked array[%ld] = [%d]\n", i, tmp);
-            if (array[i] == value)
-                return (i);
-        }
-    }
-    return (-1);
+if (array != NULL)
+{
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+}
+}
+return (-1);
 }
